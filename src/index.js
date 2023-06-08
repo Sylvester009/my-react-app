f5e88326807417c78d5df37f7ac01a6845625eea
Samuel Sylvester <sasy260902@gmail.com>
2023-06-08 20:56:17 +0100

    TwitterClone

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Nav from "./pages/Nav.js";
+import "./index.css";
+import "./mySass.scss";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export default function App() {
+  return (
+    <div className="nav">
+      <Nav />
+    </div>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const container = document.getElementById("nav");
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
---
+++ src/pages/Nav.js
@@ -0,0 +1,7 @@
+function Nav(){
+    return (
+    <h1>testing</h1>
+    );
+};
+
+export default Nav;
